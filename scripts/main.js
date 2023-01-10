6718ea745c5a7621de3850e81a523938bc8a6934
Jay Medina <jay.medina.celis@gmail.com>
2023-01-10 14:07:05 -0500

    Find items that cost between .00 and .00 USD

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,25 @@
     // Return the average
     return avg;
   }
+  function gatherPriceRange(arr) {
+    return arr.filter((obj) => {
+      if (
+        obj["currency_code"] === "USD" &&
+        obj["price"] >= 14.0 &&
+        obj["price"] <= 18.0
+      ) {
+        return obj;
+      }
+    });
+  }
+
   console.log(
     "The average price is $",
+    // Round the average to the nearest 100ths place
     Math.round((calcAvgPrice(items) + Number.EPSILON) * 100) / 100
+  );
+  console.log(
+    "Items that cost between $14.00 and $18.00 USD",
+    gatherPriceRange(items)
   );
 })();
